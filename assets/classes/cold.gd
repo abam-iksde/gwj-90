df08f146e4ab347746d07d3c280e85a98aad5e0b
Adam Sadlik <assdlik@gmail.com>
2026-02-20 01:39:16 +0100

    move deplete rates to constants

--- assets/classes/cold.gd
+++ assets/classes/cold.gd
@@ -2,13 +2,9 @@ class_name Cold
 extends Node
 
 
-var deplete_outside_per_second = 1.1
-var deplete_inside_per_second = 0.5
-
-
 func _physics_process(delta: float) -> void:
 	GameState.game_data.player_health -= (
-		(deplete_inside_per_second if GameState.plow.player_in else deplete_outside_per_second)
+		(Constants.HEALTH_DEPLETE_INSIDE if GameState.plow.player_in else Constants.HEALTH_DEPLETE_OUTSIDE)
 		* delta
 		* GameState.environment.current_time_of_day.cold_scale
 	)
