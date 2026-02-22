c24298e908e57b173764319de00806ffbaff5a0a
Adam Sadlik <assdlik@gmail.com>
2026-02-22 13:16:19 +0100

    go to menu with a fadeout on win

--- assets/prefabs/ui/win-screen/win_screen.gd
+++ assets/prefabs/ui/win-screen/win_screen.gd
@@ -11,7 +11,13 @@ func on_hide():
 
 
 func _ready():
-	(get_node('button-exit') as Button).pressed.connect(get_tree().quit)
+	(get_node('button-exit') as Button).pressed.connect(func():
+		var tween: Tween = get_node('../fade').fade_out()
+		tween.tween_callback(func():
+			get_tree().paused = false
+			get_tree().change_scene_to_file('res://assets/scene/main-menu.tscn')
+		)
+	)
 	(get_node('button-continue') as Button).pressed.connect(func():
 		Sounds.play_click()
 		get_tree().paused = false
