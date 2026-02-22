ecfb7b73f36ab35699bbad3e3b3e961558ac3c26
Adam Sadlik <assdlik@gmail.com>
2026-02-22 18:37:32 +0100

    rebalance game values

--- assets/prefabs/ui/hud/gas-tank/gas-tank-bar.gd
+++ assets/prefabs/ui/hud/gas-tank/gas-tank-bar.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 const MIN_PROGRESS = 7.7
-const MAX_PROGRESS = 90.6
+const MAX_PROGRESS = 92.3
 
 
 @onready var progress_bar: TextureProgressBar = get_node('texture-progress-bar')
